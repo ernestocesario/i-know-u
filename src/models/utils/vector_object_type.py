09eb6fa74ebbd7e9d14a35ddd351127fb6acc379
ernestocesario <106409947+ernestocesario@users.noreply.github.com>
2026-01-10 19:14:39 +0100

    Fix: move _generate_next_value in vector_object_type.py to the top

--- src/models/utils/vector_object_type.py
+++ src/models/utils/vector_object_type.py
@@ -1,11 +1,11 @@
 from enum import Enum, auto
 
 class VectorObjectType(str, Enum):
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values):
+        return name
+
     PROFILE = auto()
     STORY = auto()
     POST = auto()
     HIGHLIGHT = auto()
-
-    @staticmethod
-    def _generate_next_value_(name, start, count, last_values):
-        return name
